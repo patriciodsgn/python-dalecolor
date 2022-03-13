1b2f9caba58ad8e3b676aeb28bdd2805376b9b12
patricio-dsgn <74270376+patricio-dsgn@users.noreply.github.com>
2022-03-12 21:28:17 -0300

    ok

--- demo.py
+++ demo.py
@@ -1,15 +1,13 @@
-import python_console_style as uxi
+import dalecolor as dc
 
 
 # text with f()
-print( uxi.f("hello im a red text", "red"))
-print( uxi.f("hello im a text with green background", "GREEN"))
-print( uxi.f("hello im a cyan text in bold", "bold", "cyan"))
+print( dc.f("hello im a red text", "red"))
+print( dc.f("hello im a text with green background", "GREEN"))
+print( dc.f("hello im a cyan text in bold", "bold", "cyan"))
 
 
 # text with jumbo()
-print(uxi.jumbo("hello im a jumbo block normal", 1))
-print(uxi.jumbo("hello im a jumbo block medium", 2))
-print(uxi.jumbo("hello im a jumbo block large", 3))
-
-
+print(dc.jumbo("hello im a jumbo block normal", 1))
+print(dc.jumbo("hello im a jumbo block medium", 2))
+print(dc.jumbo("hello im a jumbo block large", 3))
