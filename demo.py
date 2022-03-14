c155b11cfc1107a6503301a8ebd53c6abae525a3
patricio-dsgn <74270376+patricio-dsgn@users.noreply.github.com>
2022-03-14 18:25:04 -0300

    ok

--- demo.py
+++ demo.py
@@ -1,13 +1,36 @@
 import dalecolor as dc
 
 
-# text with f()
+# print text with f()
 print( dc.f("hello im a red text", "red"))
 print( dc.f("hello im a text with green background", "GREEN"))
 print( dc.f("hello im a cyan text in bold", "bold", "cyan"))
 
 
-# text with jumbo()
+# print text with jumbo()
 print(dc.jumbo("hello im a jumbo block normal", 1))
 print(dc.jumbo("hello im a jumbo block medium", 2))
 print(dc.jumbo("hello im a jumbo block large", 3))
+
+
+# print list with table()
+demo_list = [
+    ['designer', 'fireman', 'dancer', 'teacher', 'doctor'],
+    ['red', 'green', 'yellow', 'rainbow', 'silver'],
+    ['a', 'e', 'i', 'o', 'u'],
+    ['one', 'two', 'three', 'eleven', 'hundred']        
+]
+
+dc.table(
+    demo_list,
+    head = ['COL1', 'TWO', 'THREE', 'FOUR', '5'],
+    head_style = ['bold', 'red', 'WHITE'],
+    style = ['italic', 'yellow', 'BLUE'],
+    align = 'center',
+    padding = '   ',
+    margin = ' - '
+    )
+
+
+
+
